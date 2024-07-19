07248f701ff1a3a158b76c6cfc7e759c26c551e5
Johongir10 <shavkatovj73@gmail.com>
2024-07-19 11:18:49 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,20 @@
+a = prompt("Birnichi sonni kiriting")
+b = prompt("Ikkinchi sonni kiriting")
+
+
+
+function min(){
+    if(a<b){
+        alert(a+"soni" + b+ "sonidan kichik")
+    } else if (a = b) {
+        alert(a + "soni" + b + "soni bilan teng")
+    }
+    else{
+        alert(b+"soni" +a +" sonidan kichik")
+    }
+}
+
+
+
+min()
+   
